e63ef41be045096bc614ab2d6b08458de163bf74
martwaters <martin.kruse@waters.com>
2016-04-27 11:17:51 +0200

    Added User Put

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,24 +2,53 @@
 using System.Linq;
 using System.Web.Http;
 using HerrOber2.Models;
+using System;
 
 namespace HerrOber2.Controllers
 {
-    public class UsersController : ApiController
+    public class UsersController : BaseController
     {
         [Route("users")]
-        public IEnumerable<User> Get()
+        public IHttpActionResult Get()
         {
-            return DataModel.Instance.Users;
+            return Ok(DataModel.Instance.Users);
         }
 
         [Route("users({emailAddress})")]
-        public User Get(string emailAddress)
+        public IHttpActionResult Get(string emailAddress)
         {
             User user = DataModel.Instance.Users.FirstOrDefault(x => x.EmailAddress.Equals(emailAddress));
-            //if (user == null)
-                //return NotFound();
-            return user;
+            if (user == null)
+                return NotFound();
+            return Ok(user);
         }
+
+        [HttpPut]
+        [Route("users")]
+        public IHttpActionResult Put([FromBody] User user)
+        {
+            //User user = DataModel.Instance.Users.FirstOrDefault(x => x.EmailAddress.Equals(emailAddress));
+            if (user == null)
+                return BadRequest();
+            try
+            {
+                User existingUser = DataModel.Instance.Users.FirstOrDefault(x => x.EmailAddress.Equals(user.EmailAddress));
+                if (existingUser == null)
+                    DataModel.Instance.Users.Add(user);
+                else
+                {
+                    int index = DataModel.Instance.Users.IndexOf(existingUser);
+                    DataModel.Instance.Users[index] = user;
+                }
+
+                return OkNoContent();
+            }
+            catch (Exception ex)
+            {
+                // System.Web.Http.Results
+                return InternalServerError(ex);
+             }
+        }
+
     }
 }

6af9a2cdfb7f30044f7301348f2355a2075c8b70
wolfwaters <wolfgang.foerster@waters.com>
2016-05-10 16:49:08 +0200

    center image in carousel

--- WebPage/Users.js
+++ WebPage/Users.js
@@ -3,17 +3,20 @@ $(document).ready(function () {
     var baseAddress = 'http://localhost:4711/';
 
     $.getJSON(baseAddress + 'users', function (users) {
+
+        //--- loop through all users to fill the carousel
         for (i = 0; i < users.length; i++) {
+
             //--- add indicators
-            $('<li data-target="#carousel-example-generic" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators')
+            $('<li data-target="#myCarousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators')
 
             //--- add items
-            var imageUrl = 'http://herrober.rdeadmin.waters.com/Unknown.jpg';
-            $('<div class="item"><img src="' + imageUrl + '"><div class="carousel-caption"><h3>' + users[i].DisplayName + '</h3><p>' + users[i].EmailAddress + '</p></div></div>').appendTo('.carousel-inner');
+            var imageUrl = 'http://herrober.rdeadmin.waters.com/Unknown2.jpg';
+            $('<div class="item"><img class="img-responsive center-block" src="' + imageUrl + '"><div class="carousel-caption"><h3>' + users[i].DisplayName + '</h3><p>' + users[i].EmailAddress + '</p></div></div>').appendTo('.carousel-inner');
         }
 
         $('.item').first().addClass('active');
         $('.carousel-indicators > li').first().addClass('active');
-        $('#carousel-example-generic').carousel();
+        $('#myCarousel').carousel();
     });
 });
